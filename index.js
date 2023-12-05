07753da01a3e9d9025b53a291004b2341ae8a4b7
laskarsaif <145012138+laskarsaif@users.noreply.github.com>
2023-12-05 16:53:01 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const Name = document.getElementById("name")
-const Class = document.getElementById("Class")
+const Namee = document.getElementById("name")
+const Cclass = document.getElementById("Class")
 const Email = document.getElementById("Email")
 const StudentId = document.getElementById("StudentId")
 const Password = document.getElementById("Password")
 let SubmitBtn = document.getElementById("SubmitBtn")
 let LoginBtn = document.getElementById("LoginBtn")
 
-
-function submit(){
+document.getElementById("SubmitBtn").onclick = function(){
+  let Identity = {
+    Name: document.getElementById("name").value,
+    Cclass: document.getElementById("Class").value,
+    Email: document.getElementById("Email").value,
+    StudentId: document.getElementById("StudentId").value,
+    Password: document.getElementById("Password").value,
+  }
 }
