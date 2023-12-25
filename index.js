1a55518f782ec193f1cb42625ce91a23261e268a
laskarsaif <145012138+laskarsaif@users.noreply.github.com>
2023-12-25 23:13:10 +0600

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ document.getElementById("SubmitBtn").onclick = function(){
     StudentId: document.getElementById("StudentId").value,
     Password: document.getElementById("Password").value,
   }
+  console.log(Identity)
 }
 
 document.getElementById("SubmitBtn").onclick = function(){
