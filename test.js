6f5a2b9256a3170f7a991cc2e4589631f76f6f70
laskarsaif <145012138+laskarsaif@users.noreply.github.com>
2023-12-25 23:19:42 +0600

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 let info = window.object
 console.log("pleaaaaaaaaaaaaaaaaaaasssssssseeeeee make it work")
-console.log("will node js work in this runtime environment")
+console.log("will node js work in this runtime environment?")
 console.log(info.Name)
