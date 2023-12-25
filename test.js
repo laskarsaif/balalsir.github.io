e7bbd0e7cc80f16463e82755834936bbb5fbed9e
laskarsaif <145012138+laskarsaif@users.noreply.github.com>
2023-12-26 00:02:46 +0600

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 let info = window.object
+console.log(info ? info.Name : "Identity not yet defined")
 console.log("pleaaaaaaaaaaaaaaaaaaasssssssseeeeee make it work")
 console.log("will node js work in this runtime environment?")
 console.log(info.Name)
