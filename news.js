29bb3ed04de0c629d280e0eab9b1592a7c4ee5c1
laskarsaif <145012138+laskarsaif@users.noreply.github.com>
2024-01-06 12:41:15 +0600

    Update news.js

--- news.js
+++ news.js
@@ -1,1 +1,31 @@
+let newsone = document.querySelector("#newsone")
+let newstwo = document.querySelector("#newsone")
+let newsthree = document.querySelector("#newsone")
+let newsfour = document.querySelector("#newsone")
 
+function editannouncement(){
+    newnews = window.prompt("write new announcement")
+    if(newsone == ""){
+        newsone.textContent = newnews
+    }
+    else if(newsone != "" && newstwo == ""){
+        newstwo.textContent = newnews
+    }
+    else if(newsone != "" && newstwo != "" && newsthree == ""){
+        newsthree.textContent = newnews
+    }
+    else if(newsone != "" && newstwo != "" && newsthree != "" && newsfour == ""){
+        newsfour.textContent = newnews
+    }
+    else{
+        newsone.textContent = newnews
+    }
+
+}
+function resetnews(){
+    newsone.textContent = ""
+    newstwo.textContent = ""
+    newsthree.textContent = ""
+    newsfour.textContent = ""
+}
+setInterval(resetnews, 604800000)
